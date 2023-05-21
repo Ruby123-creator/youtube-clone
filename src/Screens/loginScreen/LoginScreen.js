045e205cc67b98879634600a7e7d6c3a3fe93ee0
Your Name <you@example.com>
2023-05-21 17:52:07 +0530

    module

--- src/Screens/loginScreen/LoginScreen.js
+++ src/Screens/loginScreen/LoginScreen.js
@@ -1,6 +1,6 @@
 import './loginScreen.css'
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch ,useSelector} from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { Login } from '../../Redux/Actions/Authaction'
 
@@ -8,16 +8,16 @@ import { Login } from '../../Redux/Actions/Authaction'
 const LoginScreen = () => {
 
  const dispatch = useDispatch()
-//  const accessToken = useSelector(state=>state.auth.accessToken)
+ const accessToken = useSelector(state=>state.auth.accessToken)
  const handleLogin =()=>{
    dispatch(Login)
  }
  const Navigate = useNavigate();
  useEffect(()=>{
-   if(sessionStorage.getItem("userToken")){
+   if(accessToken && sessionStorage.getItem("userToken")){
       Navigate('/')
    }
- },[Navigate])
+ },[accessToken])
 
 
    return (
